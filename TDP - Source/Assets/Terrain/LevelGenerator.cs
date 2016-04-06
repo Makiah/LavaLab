7d00a734433dac8f5136f8e9ecadf3218abcb412
Makiah <makiahtbennett@gmail.com>
2016-04-05 20:31:49 -0400

    Added quite a few changes that actually look pretty good so far (except the mid things do not have an opacity layer).  Sticking with KISS (Keep It Simple Stupid) and it is working pretty well.  The LevelGenerator script should be a bit smarter, as should the game mechanics as a whole.  Instead of going crazy and instantiating a crap ton of levels all at once, the level generator script takes in a level number, and generates a level based off of the ID.  With the turrets working eventually, it should be a lot of fun to play.  The one problem is making it look as though the player is moving upward when he is really staying in the same spot (the magic of animators), moving the player down below the newly generated level, and then moving the player up to the level when the level has finished loading and at least 6 seconds have passed.

---
+++ TDP - Source/Assets/Terrain/LevelGenerator.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Here is the way that I am envisioning levels being generated.  To begin, only ONE level is created.  The player completes the level, then moves into the elevator.  
+ * Then the level is destroyed, and while the animation is playing to make it look like the player is moving upward, the elevator is actually below where it was 
+ * while the new level is being loaded.  When the level is done loading and at least 6 seconds have passed, then the elevator "arrives" at its destination on the 
+ * same vertical level as the previous one.  
+ * 
+ * Also, just a thought, why not make it so that turrets can be instantiated by going new Turret(location, fireRate, etc.), which will instantiate a turret at the 
+ * given location and initialize it?  It would have to be a bit weird since the already existent class could not be added (I think).  
+ */
+
+public class LevelGenerator : MonoBehaviour {
+
+	//By right elevator room and left elevator room I mean the direction you have to walk to get into the elevator.  
+	[SerializeField] private GameObject startRoom = null, midRoom = null, leftElevatorRoom = null, rightElevatorRoom = null, leftReceiverRoom = null, rightReceiverRoom = null;
+
+	void Start() {
+		CreateLevel (15);
+	}
+
+	//Keep track of the current x location across all classes.  
+	private float currentXLocation = 0f;
+
+	public void CreateLevel(int levelID) {
+		if (levelID < 1) {
+			Debug.LogError ("Only levels 1 to infinity are valid level IDs");
+			levelID = 1;
+		}
+
+		//Count the number of segments placed.  
+		int segmentsPlaced = 0;
+		int direction = (levelID % 2 == 0 ? 1 : -1);
+
+		if (levelID == 1) {
+			//Place the receiver left or right based on the level.  Remember than even levels move left to right and odd levels move right to left.  
+			PlaceTerrain (startRoom, new Vector2(currentXLocation, 0));
+			//Displace the x location by either a negative or a positive value based on the level ID.  
+			currentXLocation += GetSpriteSize (startRoom).x / 2f * direction;
+		} else {
+			//Place the receiver left or right based on the level.  Remember than even levels move left to right and odd levels move right to left.  
+			PlaceTerrain (levelID % 2 == 0 ? rightReceiverRoom : leftReceiverRoom, new Vector2(currentXLocation, 0));
+			//Displace the x location by either a negative or a positive value based on the level ID.  
+			currentXLocation += GetSpriteSize (rightReceiverRoom).x / 2f * direction;
+		}
+		//Increment the number of segments placed by 1 (for the receiver).  
+		segmentsPlaced++;
+
+		//Level length increases by 1 every 2 levels.  
+		while (segmentsPlaced < 3 + levelID / 2 - 1) {
+			currentXLocation += GetSpriteSize (midRoom).x / 2f * direction;
+			PlaceTerrain (midRoom, new Vector3 (currentXLocation, 0, 0));
+			currentXLocation += GetSpriteSize (midRoom).x / 2f * direction;
+
+			//Increment segmentsPlaced.  
+			segmentsPlaced++;
+		}
+
+		//Place the receiver left or right based on the level.  Remember than even levels move left to right and odd levels move right to left.  
+		PlaceTerrain (levelID % 2 == 0 ? rightElevatorRoom : leftElevatorRoom, new Vector2(currentXLocation, 0));
+		//Increment the number of segments placed by 1 (for the receiver).  
+		segmentsPlaced++;
+
+	}
+
+	//Used to get the size of a sprite from a GameObject without the irritation factor.  
+	private Vector2 GetSpriteSize(GameObject obj) {
+		if (obj.GetComponent <SpriteRenderer> () != null) {
+			return obj.GetComponent <SpriteRenderer> ().bounds.size;
+		} else {
+			Debug.LogError (obj.name + " does not have a SpriteRenderer attached!");
+			return Vector2.zero;
+		}
+	}
+
+	private GameObject PlaceTerrain(GameObject terrain, Vector2 location) {
+		return (GameObject) (Instantiate (terrain, location, Quaternion.identity));
+	}
+
+}
